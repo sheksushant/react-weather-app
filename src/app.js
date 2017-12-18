f177dd20b55c74a66a036784842d8f2a3e0f8ab8
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-12-18 11:44:50 +0530

    setup bootstrap

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WeatherApp from './components/WeatherApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<WeatherApp />, document.getElementById('app'));
